f0c83c826748409bfd9b3ef80883c932f58d8c09
Uenderss <meajudabarto@gmail.com>
2022-08-09 21:13:32 -0300

    correcao do index.js principal

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express, { json } from "express";
 import cors from "cors";
+import router from "./routes/index.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+
 const app = express();
-app.use(cors(), json());
+app.use(cors(), json(),router);
+
 
 app.listen(process.env.PORT, () => console.log(`Server is listening on port ${process.env.PORT}`));
