4bf14069f1d0477c9020efcfdfa2a6b10bc580e6
Tiago <tigolimab223@gmail.com>
2022-08-15 12:12:12 -0300

    small bugfix in the postController

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -2,9 +2,8 @@ import { postRepository } from "../repositories/postRepository.js";
 
 export async function postData(req, res) {
     const { user_id, url, description } = req.body;
-    let cleanUrl = url.trim();
     try {
-        await postRepository.postData(user_id, cleanUrl, description);
+        await postRepository.postData(user_id, url, description);
         return res.sendStatus(201);
     } catch (error) {
         return res.send(error.message).status(500);
